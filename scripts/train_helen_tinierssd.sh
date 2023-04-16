7b7af046d80d317374e94d7977a59a31e1b0da7f
MarielTinaco <mariel.tinaco@analog.com>
2023-04-16 16:56:01 +0800

    Updated files for eye detection training

--- scripts/train_helen_tinierssd.sh
+++ scripts/train_helen_tinierssd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --deterministic --print-freq 200 --epochs 150 --optimizer Adam --lr 0.001 --model ai85tinierssd --use-bias --momentum 0.9 --weight-decay 5e-4 --dataset HELEN_74 --device MAX78000 --obj-detection --obj-detection-params parameters/obj_detection_params_svhn.yaml --batch-size 64 --qat-policy policies/qat_policy_svhn.yaml --enable-tensorboard --validation-split 0 "$@"
+python train.py --deterministic --print-freq 200 --epochs 300 --optimizer Adam --lr 0.1e-5 --model ai85tinierssd --use-bias --momentum 0.9 --weight-decay 5e-4 --dataset HELEN_74 --device MAX78000 --obj-detection --obj-detection-params parameters/obj_detection_params.yaml --batch-size 128 --qat-policy policies/qat_policy_svhn.yaml --enable-tensorboard --validation-split 0"$@"
