c55db155bd9b92ce8d6bb5a1e53c5640a5226727
Mariel <mariel.tinaco@analog.com>
2024-01-23 22:45:59 +0800

    Add: NILM dataloader

---
+++ datasets/nilm.py
@@ -0,0 +1,122 @@
+import numpy as np
+import torch
+
+import torchvision
+from torchvision import transforms
+
+from pathlib import Path
+
+import ai8x
+
+def split_ukdale(data):
+    split_1 = int(0.60 * len(data))
+    split_2 = int(0.85 * len(data))
+    train = data[:split_1]
+    validation = data[split_1:split_2]
+    test = data[split_2:]
+    return train, validation, test
+
+def load_data(data_path, data_type="training", sample=None, data="ukdale", denoise=False):
+    if denoise:
+        x = np.load(data_path+f"/{data}/{data_type}/denoise_inputs.npy")
+    else:
+        x = np.load(data_path+f"/{data}/{data_type}/noise_inputs.npy")
+    y = np.load(data_path+f"/{data}/{data_type}/targets.npy")
+    z = np.load(data_path+f"/{data}/{data_type}/states.npy")
+    if sample is None:
+        return x, y, z
+    else:
+        return x[:sample], y[:sample], z[:sample]
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self,  inputs, targets, states,  seq_len=99):
+        self.inputs = inputs
+        self.targets = targets
+        self.states  = states
+        seq_len = seq_len  if seq_len% 2==0 else seq_len+1
+        self.seq_len = seq_len
+        self.len = self.inputs.shape[0] - self.seq_len
+        self.indices = np.arange(self.inputs.shape[0])
+
+    def __len__(self):
+        'Denotes the total number of samples'
+        return self.len
+    
+    def get_sample(self, index):
+        indices = self.indices[index : index + self.seq_len]
+        inds_inputs=sorted(indices[:self.seq_len])
+        inds_targs=sorted(indices[self.seq_len-1:self.seq_len])
+
+        return self.inputs[inds_inputs], self.targets[inds_targs], self.states[inds_targs]
+
+    def __getitem__(self, index):
+        inputs, target, state = self.get_sample(index)
+        return torch.tensor(inputs).unsqueeze(-1).float(), torch.tensor(target).float().squeeze(), torch.tensor(state).long().squeeze()
+
+class UkdaleDataset(Dataset):
+
+    def __init__(self, root_dir, d_type, transform=None, seq_len=99, denoise=False):
+        
+        inputs, targets, states = load_data(data_path=str(root_dir),
+                                data="ukdale",
+                                data_type="training",
+                                denoise=denoise
+                                )
+        inputs_train, inputs_val, inputs_test = split_ukdale(inputs)
+        targets_train, targets_val, targets_test = split_ukdale(targets)
+        states_train, states_val, states_test = split_ukdale(states)
+        
+        if d_type == "train":
+            inputs = inputs_train
+            targets = targets_train
+            states = states_train
+        elif d_type == "test":
+            inputs = inputs_test
+            targets = targets_test
+            states = states_test
+        elif d_type == "val":
+            inputs = inputs_val
+            targets = targets_val
+            states = states_val
+
+        super().__init__(inputs, targets, states, seq_len)
+
+
+def ukdale_get_datasets(data, load_train=True, load_test=True):
+
+    (data_dir, args) = data
+
+    if load_train:
+        train_transform = transforms.Compose([
+            ai8x.normalize(args=args)
+        ])
+
+        train_dataset = UkdaleDataset(root_dir=data_dir,
+                                      d_type="train",
+                                      transform=train_transform)
+        
+    else:
+        train_dataset = None
+
+    if load_test:
+        test_transform = transforms.Compose([
+            ai8x.normalize(args=args)
+            ]
+        )
+
+        test_dataset = UkdaleDataset(root_dir=data_dir,
+                                     d_type="test",
+                                     transform=test_transform)
+    else:
+        test_dataset = None
+    
+    return train_dataset, test_dataset
+
+datasets = [
+    {
+        'name': 'ukdale',
+        'input' : (128),
+        'output' : (),
+        'loader' : ukdale_get_datasets,
+    }
+]
