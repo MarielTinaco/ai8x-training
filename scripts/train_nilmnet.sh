e8e6710a4ba2c38373be0ae841e02ad0daac0fb0
Mariel Tinaco <mariel.tinaco@analog.com>
2024-04-25 17:02:59 +0800

    Add: NILM Autoencoder setup

--- scripts/train_nilmnet.sh
+++ scripts/train_nilmnet.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-python train.py --epochs 100 --optimizer Adam --lr 0.001 --deterministic --compress policies/schedule-nilm.yaml --multitarget --model ai85nilmnet --batch-size 256 --dataset UKDALE_small --multitarget --qat-policy policies/qat_policy_cnn1dnilm.yaml --enable-tensorboard --use-bias --device MAX78000 "$@"
+# python train.py --epochs 100 --optimizer Adam --lr 0.001 --deterministic --compress policies/schedule-nilm.yaml --multitarget --model ai85nilmnet --batch-size 256 --dataset UKDALE_small --multitarget --qat-policy policies/qat_policy_cnn1dnilm.yaml --enable-tensorboard --use-bias --device MAX78000 "$@"
+
+python train.py --epochs 100 --optimizer Adam --lr 0.001 --deterministic --compress policies/schedule-nilm.yaml --multitarget --model ai85nilmautoencoder --batch-size 256 --dataset UKDALE_small_autoencoder --multitarget --qat-policy policies/qat_policy_nilm_autoencoder.yaml --enable-tensorboard --use-bias --device MAX78000 "$@"
