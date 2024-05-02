bc4764766fb4b42e9e65998c2d83f830d7684208
Mariel Tinaco <mariel.tinaco@analog.com>
2024-05-02 15:34:02 +0800

    Add: NILM Seq2Point

---
+++ scripts/train_nilm_seq2point.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python train.py --epochs 100 --optimizer Adam --lr 0.001 --deterministic --compress policies/schedule-nilm.yaml --multitarget --model ai85nilmseq2point --batch-size 256 --dataset UKDALE_small --multitarget --qat-policy policies/qat_policy_cnn1dnilm.yaml --enable-tensorboard --use-bias --device MAX78000 "$@"
