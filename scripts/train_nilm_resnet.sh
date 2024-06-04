69a8ec335c522cf6eed216673c23004bddccdcd9
Mariel Tinaco <mariel.tinaco@analog.com>
2024-06-04 09:28:17 +0800

    Update: Train script for Resnet+Seq2Point v1

---
+++ scripts/train_nilm_resnet.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python train.py --epochs 200 --optimizer Adam --lr 0.001 --deterministic --compress policies/schedule-nilm-seq2point.yaml --model ai85nilmresidual --batch-size 256 --dataset UKDALE_small_regress --multitarget --qat-policy policies/qat_policy_nilm_seq2point.yaml --enable-tensorboard --use-bias --device MAX78000 "$@"
