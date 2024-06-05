ca06cb7eb310d744350ba0c447a9ad6bd916bdd6
Mariel Tinaco <mariel.tinaco@analog.com>
2024-06-05 14:50:26 +0800

    Add: TCN+Seq2Point arch

---
+++ models/ai85net_nilm_seq2point_tcn.py
@@ -0,0 +1,155 @@
+###################################################################################################
+#
+# Copyright (C) 2021 Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+"""
+Seq2Point network for MAX7800X
+"""
+import torch
+from torch import nn
+
+import torch.nn.functional as F
+
+import ai8x
+
+
+class AI85NILMSeq2PointRegress(nn.Module):
+    """
+    Small size UNet model
+    """
+    def __init__(
+            self,
+            num_classes=5,
+            num_channels=1,
+            dimensions=(100, 1),
+            window_len=100,
+            dropout=0.20,
+            bias=True,
+            **kwargs
+    ):
+        super().__init__()
+
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+        self.dropout4 = nn.Dropout(dropout)
+
+        self.add1 = ai8x.Add()
+        self.add2 = ai8x.Add()
+
+        self.conv1 = ai8x.FusedConv1dBNReLU(num_channels, 64, 3, stride=1, padding=1,
+                dilation=1, bias=bias, batchnorm='Affine', **kwargs)
+
+        self.conv2 = ai8x.FusedConv1dBNReLU(64, 64, 3, stride=1, padding=1,
+                dilation=1, bias=bias, batchnorm='Affine', **kwargs)
+
+        self.conv3 = ai8x.FusedConv1dBNReLU(64, 64, 3, stride=1, padding=1,
+                dilation=1, bias=bias, batchnorm='Affine', **kwargs)
+
+        self.conv4 = ai8x.FusedConv1dBNReLU(64, 64, 3, stride=1, padding=2,
+                dilation=2, bias=bias, batchnorm='Affine', **kwargs)
+
+        self.conv5 = ai8x.FusedConv1dBNReLU(64, 64, 3, stride=1, padding=2,
+                dilation=2, bias=bias, batchnorm='Affine', **kwargs)
+
+        self.conv6 = ai8x.FusedConv1dBNReLU(64, 64, 3, stride=1, padding=2,
+                dilation=4, bias=bias, batchnorm='Affine', **kwargs)
+        
+        self.conv7 = ai8x.FusedConv1dBNReLU(64, 64, 3, stride=1, padding=2,
+                dilation=4, bias=bias, batchnorm='Affine', **kwargs)
+
+        self.conv8 = ai8x.FusedMaxPoolConv1dBNReLU(64, 96, 1, pool_size = 2, stride=1, padding=0,
+                dilation=1, bias=bias, batchnorm='Affine', **kwargs)
+        
+        self.conv9 = ai8x.FusedAvgPoolConv1dBNReLU(96, 32, 1, pool_size = 2, stride=1, padding=0,
+                dilation=1, bias=bias, batchnorm='Affine', **kwargs)
+        
+        self.mlp1 = ai8x.FusedLinearReLU(32*23, 256, bias=bias, **kwargs)
+
+        self.fc_state = ai8x.Linear(256, num_classes*2, bias=bias, **kwargs)
+        self.fc_power = ai8x.Linear(256, num_classes*5, bias=bias, **kwargs)
+
+        self.initWeights("kaiming")
+
+    def forward(self, x):
+        # Initial 3x1 convolution 
+        x_res = self.conv1(x)                   # 64 x 100
+
+        # TCN block 1
+        x = self.conv2(x_res)                   # 64 x 100
+        x = self.conv3(x)                       # 64 x 100
+        x = self.dropout1(x)
+        x_res = self.add1(x, x_res)             # 64 x 100
+
+        # TCN block 2
+        x = self.conv4(x_res)                   # 64 x 100
+        x = self.conv5(x)                       # 64 x 100
+        x = self.dropout2(x)
+        x_res = self.add2(x, x_res)             # 64 x 100
+
+        # TCN block 3 (but without skip connection due to padding constraints. Consider adding conv 1x1 for skip)
+        x = self.conv6(x_res)                   # 64 x 92
+        x = self.conv7(x)                       # 64 x 84 
+        x = self.dropout3(x)
+
+        # Downsample
+        x = self.conv8(x)                       # 96 x 42
+        x = self.conv9(x)                       # 32 x 21
+        x = self.dropout4(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.mlp1(x)
+        x1 = self.fc_state(x)
+        x1 = x1.view(x1.size(0), -1)
+        x2 = self.fc_power(x)
+        x2 = x2.view(x2.size(0), -1)
+        return torch.cat([x1, x2], dim=1)
+
+    def initWeights(self, weight_init="kaiming"):
+        """
+        Auto Encoder Weight Initialization
+        """
+        weight_init = weight_init.lower()
+        assert weight_init in ('kaiming', 'xavier', 'glorot')
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                if weight_init == "kaiming":
+                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+                elif weight_init in ('glorot', 'xavier'):
+                    nn.init.xavier_uniform_(m.weight)
+
+            elif isinstance(m, nn.ConvTranspose2d):
+                if weight_init == "kaiming":
+                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+                elif weight_init in ('glorot', 'xavier'):
+                    nn.init.xavier_uniform_(m.weight)
+
+            elif isinstance(m, nn.Linear):
+                if weight_init == "kaiming":
+                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+                elif weight_init in ('glorot', 'xavier'):
+                    nn.init.xavier_uniform_(m.weight)
+
+def ai85nilmtcn(pretrained=False, **kwargs):
+    """
+    Constructs a AI85KWS20Net model.
+    """
+    assert not pretrained
+    return AI85NILMSeq2PointRegress(**kwargs)
+
+
+models = [
+    {
+        'name': 'ai85nilmtcn',
+        'min_input': 1,
+        'dim': 1,
+    },
+]
