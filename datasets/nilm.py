25f4e5ab5322517a0f97e262d5ceff543a4dd612
Mariel Tinaco <mariel.tinaco@analog.com>
2024-09-11 02:50:46 +0800

    Refactor: Revised dataloader for new dataset

--- datasets/nilm.py
+++ datasets/nilm.py
@@ -3,144 +3,108 @@ import torch
 
 import torchvision
 from torchvision import transforms
+from sklearn.preprocessing import minmax_scale
 
 from pathlib import Path
 
 import ai8x
 
-PROFILE_NAME = "unetnilm_ukdale_20240321_155419"
-EXTENSION_MODE = "randomizer"
+class Seq2PointNILM(torch.utils.data.Dataset):
 
-def split_ukdale(data):
-    if EXTENSION_MODE == "appender":
-        split_1 = int(0.15 * len(data))
-        split_2 = int(0.75 * len(data))
-        validation = data[:split_1]
-        train = data[split_1:split_2]
-        test = data[split_2:]
-    else:
-        split_1 = int(0.60 * len(data))
-        split_2 = int(0.85 * len(data))
-        train = data[:split_1]
-        validation = data[split_1:split_2]
-        test = data[split_2:]
-    return train, validation, test
-
-def load_data(data_path, data_type="training", sample=None, data="ukdale", denoise=False):
-    if denoise:
-        x = np.load(data_path+f"/{data}/{PROFILE_NAME}/{data_type}/denoise_inputs.npy")
-    else:
-        x = np.load(data_path+f"/{data}/{PROFILE_NAME}/{data_type}/noise_inputs.npy")
-    y = np.load(data_path+f"/{data}/{PROFILE_NAME}/{data_type}/targets.npy")
-    z = np.load(data_path+f"/{data}/{PROFILE_NAME}/{data_type}/states.npy")
-    if sample is None:
-        return x, y, z
-    else:
-        return x[:sample], y[:sample], z[:sample]
-
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self,  inputs, targets, states,  seq_len=99):
-        self.inputs = inputs
-        self.targets = targets
-        self.states  = states
-        seq_len = seq_len  if seq_len% 2==0 else seq_len+1
+    def __init__(self, root, d_type, seq_len=100, transform=None,denoise=True):
+        self.root = Path(root)
+        self.d_type = d_type
+        self.transform = transform
         self.seq_len = seq_len
-        self.len = self.inputs.shape[0] - self.seq_len
-        self.indices = np.arange(self.inputs.shape[0])
+
+        self.data, self.labels = self.__load(denoise=denoise)
+        self.indices = np.arange(self.data.shape[0])
+
+    def __load(self, denoise=True):
+
+        if denoise:
+            x = np.load(self.root / "NILMTK" / "processed" / self.d_type / "denoise_inputs.npy" )
+        else:
+            x = np.load(self.root / "NILMTK" / "processed" / self.d_type / "noise_inputs.npy" )
+        y = np.load(self.root / "NILMTK" / "processed" / self.d_type / "targets.npy" )
+        z = np.load(self.root / "NILMTK" / "processed" / self.d_type / "states.npy" )
+
+        x = minmax_scale(x, feature_range=(0, 1))
+        z = np.apply_along_axis(lambda data : minmax_scale(data, feature_range=(0, 1)), 0, z)
+
+        return x, (y, z)
+
+    def __reshape_data(self, audio, row_len=100):
+        # add overlap if necessary later on
+        return torch.transpose(audio.reshape((-1, row_len)), 1, 0)
 
     def __len__(self):
-        'Denotes the total number of samples'
-        return self.len
-    
+        return self.data.shape[0] - self.seq_len
+
     def get_sample(self, index):
         indices = self.indices[index : index + self.seq_len]
         inds_inputs=sorted(indices[:self.seq_len])
         inds_targs=sorted(indices[self.seq_len-1:self.seq_len])
+        
+        states = self.labels[0]
+        power = self.labels[1]
 
-        return self.inputs[inds_inputs], self.targets[inds_targs], self.states[inds_targs]
+        return self.data[inds_inputs], (states[inds_targs], power[inds_targs])
 
     def __getitem__(self, index):
-        inputs, target, state = self.get_sample(index)
-        return torch.tensor(inputs).unsqueeze(-1).float(), torch.tensor(target).float().squeeze(), torch.tensor(state).long().squeeze()
+        inputs, targets = self.get_sample(index)
+        state = targets[0]
+        power = targets[1]
 
-class UkdaleDataset(Dataset):
+        inputs = torch.tensor(inputs)
+        inputs = self.__reshape_data(inputs, row_len=self.seq_len)
+        inputs = inputs.type(torch.FloatTensor)
 
-    def __init__(self, root_dir, d_type, transform=None, seq_len=99, denoise=False, data_type="training"):
-        
-        inputs, targets, states = load_data(data_path=str(root_dir),
-                                data="ukdale",
-                                data_type=data_type,
-                                denoise=denoise
-                                )
-        inputs_train, inputs_val, inputs_test = split_ukdale(inputs)
-        targets_train, targets_val, targets_test = split_ukdale(targets)
-        states_train, states_val, states_test = split_ukdale(states)
-        
-        if d_type == "train":
-            inputs = inputs_train
-            targets = targets_train
-            states = states_train
-        elif d_type == "test":
-            inputs = inputs_test
-            targets = targets_test
-            states = states_test
-        elif d_type == "val":
-            inputs = inputs_val
-            targets = targets_val
-            states = states_val
+        if self.transform:
+            inputs = self.transform(inputs)
 
-        super().__init__(inputs, targets, states, seq_len)
+        power = torch.tensor(power)
+        power = torch.transpose(power.reshape((-1, 5)), 1, 0)
+        power = power.type(torch.FloatTensor)
 
-        self.transform = transform
+        if self.transform:
+            power = self.transform(power)
 
-    def visualize_batch(self, index=None):
+        return inputs.permute(1, 0).float(), \
+            (torch.tensor(state).long().squeeze(), power.float().squeeze())
 
-        index = 0 if index is None else index
 
-        print(index)
-
-def ukdale_get_datasets(data, load_train=True, load_test=True, load_val=None):
+def seq2pointnilm_get_datasets(data, load_train=True, load_test=True):
 
     (data_dir, args) = data
 
+    transform = transforms.Compose([
+                    ai8x.normalize(args=args),
+                ])
+
     if load_train:
-        train_transform = transforms.Compose([ai8x.normalize(args)])
-        
-        train_dataset = UkdaleDataset(root_dir=data_dir,
-                                      d_type="train",
-                                      transform=train_transform)
-        
+        train_dataset = Seq2PointNILM(root=data_dir, d_type='train', 
+                                    transform=transform,
+                                    seq_len = 100)
     else:
         train_dataset = None
 
     if load_test:
-        test_transform = transforms.Compose([ai8x.normalize(args)])
-
-        test_dataset = UkdaleDataset(root_dir=data_dir,
-                                     d_type="test",
-                                     transform=test_transform)
+        test_dataset = Seq2PointNILM(root=data_dir, d_type='test',
+                                    transform=transform,
+                                    seq_len = 100)
     else:
         test_dataset = None
-    
-    if load_val:
-        val_transform = transforms.Compose([ai8x.normalize(args)])
 
-        val_dataset = UkdaleDataset(root_dir=data_dir,
-                                     d_type="val",
-                                     transform=val_transform)
-    else:
-        val_dataset = None
-
-    if load_val:
-        return train_dataset, test_dataset, val_dataset
-    else:
-        return train_dataset, test_dataset
+    return train_dataset, test_dataset
 
 datasets = [
-    {
-        'name': 'ukdale',
-        'input' : (128),
-        'output' : (),
-        'loader' : ukdale_get_datasets,
-    }
+	{
+		'name' : 'Seq2PointNILM',
+		'input' : (1, 100),
+		'output' : (0, 1, 2, 3, 4),
+		'weights' : (1, 1),
+		'loader' : seq2pointnilm_get_datasets,
+	},
 ]
+
