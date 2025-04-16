4709fb85732e2cbc65f33826f1cc9e2f66d2a800
MarielTinaco <mariel.tinaco@analog.com>
2025-04-16 14:44:58 +0800

    losses: add custom NLL loss function
    Signed-off-by: MarielTinaco <mariel.tinaco@analog.com>

---
+++ losses/nilmmultitargetloss.py
@@ -0,0 +1,50 @@
+
+import torch
+
+class QuantileLoss(torch.nn.Module):
+        def __init__(self, quantiles=[0.0025,0.1, 0.5, 0.9, 0.975]):
+                self.quantiles = quantiles
+                super().__init__() 
+        def forward(self, inputs, targets):
+                targets = targets.unsqueeze(1).expand_as(inputs)
+                quantiles = torch.tensor(self.quantiles).float().to(targets.device)
+                error = (targets - inputs).permute(0,2,1)
+                loss = torch.max(quantiles*error, (quantiles-1)*error)
+                return loss.mean()
+
+
+class NILMMultiTargetLoss(torch.nn.Module):
+    
+        def __init__(self, states_loss, quantiles=[0.0025,0.1, 0.5, 0.9, 0.975],
+                     num_classes=5,
+                        *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.quantiles = quantiles
+                self.logsoftmax = torch.nn.LogSoftmax(dim=1)
+                self.softmax = torch.nn.Softmax(dim=1)
+                self.states_loss = states_loss
+                self.rmse_loss = QuantileLoss()
+                self.num_classes = num_classes
+
+        def forward(self, inputs, targets):
+                B = inputs.size(0)
+
+                input_state = inputs[:,:2*self.num_classes].reshape(B, 2, -1)
+                input_power = inputs[:,2*self.num_classes:].reshape(B, len(self.quantiles), -1)
+
+                target_state = targets[0]
+                target_power = targets[1]
+
+                # prob, pred = torch.max(self.softmax(input_state), 1)
+                # input_power = torch.clip(input_power, min=-1, max=1)
+
+                ## States Loss
+                loss_nll = self.states_loss(self.logsoftmax(input_state), target_state)
+
+                ## Power Loss
+                # prob = prob.unsqueeze(1).expand_as(input_power)
+                loss_mse = self.rmse_loss(input_power, target_power)
+
+                loss = loss_nll + loss_mse
+
+                return loss
