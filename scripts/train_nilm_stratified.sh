ac53683f0cb2dbd7ce3fde6e7aa9ad06efaf3e60
MarielTinaco <mariel.tinaco@analog.com>
2025-06-11 09:06:12 +0800

    scripts: train_nilm_stratified: change dataset argument
    Signed-off-by: MarielTinaco <mariel.tinaco@analog.com>

--- scripts/train_nilm_stratified.sh
+++ scripts/train_nilm_stratified.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --epochs 100 --batch-size 256 --optimizer Adam --lr 0.001 --wd 0.001 --use-bias --deterministic --model ai85netnilmseq2point --dataset UKDALE_stratified --enable-nilm --multitarget --compress policies/schedule-nilm.yaml --qat-policy policies/qat_policy.yaml --device MAX78000 --validation-split 0 --compiler-mode none "$@"
+python train.py --epochs 100 --batch-size 256 --optimizer Adam --lr 0.001 --wd 0.001 --use-bias --deterministic --model ai85netnilmseq2point --dataset UKDALE_stratified_on_input --enable-nilm --multitarget --compress policies/schedule-nilm.yaml --qat-policy policies/qat_policy.yaml --device MAX78000 --validation-split 0 "$@"
