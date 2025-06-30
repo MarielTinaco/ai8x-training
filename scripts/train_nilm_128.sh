c368153958a967d605f2a36ba697460c21fd5ee1
MarielTinaco <mariel.tinaco@analog.com>
2025-06-30 14:31:04 +0800

    update training hyperparameters

--- scripts/train_nilm_128.sh
+++ scripts/train_nilm_128.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --epochs 100 --batch-size 256 --optimizer Adam --lr 0.0001 --wd 0.0001 --use-bias  --deterministic --model ai85netnilmseq2point128 --dataset UKDALE_128_stratified --enable-nilm --multitarget --compress policies/schedule-nilm.yaml --qat-policy policies/qat_policy_nilm.yaml --device MAX78000 --validation-split 0 "$@"
+python train.py --epochs 100 --batch-size 256 --optimizer Adam --lr 0.0001 --wd 0.0001 --use-bias  --deterministic --model ai85netnilmseq2point128 --dataset UKDALE_128 --enable-nilm --multitarget --compress policies/schedule-nilm.yaml --qat-policy policies/qat_policy_nilm.yaml --device MAX78000 --validation-split 0 "$@"
