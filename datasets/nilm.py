53540b88f718f540284b83e86cca40403bad7f14
MarielTinaco <mariel.tinaco@analog.com>
2025-07-07 04:59:43 +0800

    datasets: nilm: non-augmented
    Signed-off-by: MarielTinaco <mariel.tinaco@analog.com>

--- datasets/nilm.py
+++ datasets/nilm.py
@@ -668,7 +668,7 @@ def ukdale_seq2point_stratified_get_datasets(data, load_train=True, load_test=Tr
     return train_dataset, test_dataset
 
 
-def ukdale_128_seq2point_stratified_get_datasets(data, load_train=True, load_test=True):
+def ukdale_128_seq2point_aug_stratified_get_datasets(data, load_train=True, load_test=True):
 
     UKDALE_SOURCE = "ukdale_bldg1_20121109_20170426.h5"
     AUG_UKDALE_SOURCE = "ukdale_bldg1_20121109_20170426_aug.h5"
@@ -712,6 +712,52 @@ def ukdale_128_seq2point_stratified_get_datasets(data, load_train=True, load_tes
         test_dataset = None
 
     return train_dataset, test_dataset
+
+
+def ukdale_128_seq2point_stratified_get_datasets(data, load_train=True, load_test=True):
+
+    UKDALE_SOURCE = "ukdale_bldg1_20121109_20170426.h5"
+    TRAIN_TIMEFRAME = datetime(year=2014, month=3, day=25), datetime(year=2014, month=8, day=27)
+    TEST_TIMEFRAME = datetime(year=2015, month=4, day=27), datetime(year=2015, month=7, day=30)
+    (data_dir, args) = data
+
+    seq_len = 128
+    # classes = ["fridge_freezer", "kettle", "washer_dryer", "dish_washer", "microwave",
+    #            "television", "vacuum_cleaner", "toaster", "laptop_computer",
+    #            "computer", "broadband_router", "charger"]
+    classes = ["fridge_freezer", "kettle", "washer_dryer", "dish_washer", "microwave"]
+    transform = transforms.Compose([ai8x.normalize(args=args)])
+
+    if load_train:
+        train_dataset = NILM(root=data_dir,
+                             filename=UKDALE_SOURCE,
+                             classes=classes,
+                             dtype="train",
+                             transform=transform,
+                             timeframe=TRAIN_TIMEFRAME,
+                             seq_len=seq_len,
+                             synth_input=True,
+                             denoise_input=False,
+                             loading_scheme="seq2point_stratified_on_input")
+    else:
+        train_dataset = None
+
+    if load_test:
+        test_dataset = NILM(root=data_dir,
+                            filename=UKDALE_SOURCE,
+                            classes=classes,
+                            dtype="test",
+                            transform=transform,
+                            timeframe=TEST_TIMEFRAME,
+                            seq_len=seq_len,
+                            synth_input=True,
+                            denoise_input=False,
+                            loading_scheme="seq2point")
+    else:
+        test_dataset = None
+
+    return train_dataset, test_dataset
+
 
 def ukdale_128_seq2point_stratified_crossval_get_datasets(data, load_train=True, load_test=True):
 
@@ -759,7 +805,6 @@ def ukdale_128_seq2point_stratified_crossval_get_datasets(data, load_train=True,
     return train_dataset, test_dataset
 
 
-
 datasets = [
 	{
 		'name' : 'UKDALE',
@@ -800,6 +845,14 @@ datasets = [
 		'output' : (21, 26, 44, 15, 30),
 		'weight' : (1, 1),
 		'loader' : ukdale_128_seq2point_stratified_get_datasets,
+	},
+    {
+		'name' : 'UKDALE_128_stratified_aug',
+		'input' : (1, 128),
+		# 'output' : (21, 26, 44, 15, 30, 39, 43, 41, 28, 12, 8, 9),
+		'output' : (21, 26, 44, 15, 30),
+		'weight' : (1, 1),
+		'loader' : ukdale_128_seq2point_aug_stratified_get_datasets,
 	},
     {
 		'name' : 'UKDALE_128_stratified_crossval',
